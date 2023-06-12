03e2ebe928c2b150c8ced2ff013c11496a638787
Salim Badiea <37668158+SalimBadea@users.noreply.github.com>
2023-06-12 11:45:31 +0300

    init commit

---
+++ app/src/main/java/com/salem/photoeditor/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.salem.photoeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.moltaqa.everdeliver.photoeditor.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
